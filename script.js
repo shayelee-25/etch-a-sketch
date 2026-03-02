1fbe8c1095b4a578ab75a9afb74306316cbb88f7
Shaye <shayelee25@gmail.com>
2026-03-02 12:40:03 -0500

    Create functions and add event listeners

--- script.js
+++ script.js
@@ -4,24 +4,35 @@ createGridBtn.textContent = "New Grid";
 const btnContainer = document.querySelector(".btn");
 btnContainer.appendChild(createGridBtn);
 
-//contentContainer.appendChild(gridSquare);
-
 window.onload = createGrid(16);
+
 function createGrid(n) {
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
+    for (let i = 0; i < (n * n); i++) {
             let div = document.createElement("div");
             div.classList.add("square");
             div.style.width = `${960/n}px`;
-            div.style.height = `${960/n}px`;
             contentContainer.appendChild(div);
-        }
     }
 }
 
-/*
 createGridBtn.addEventListener("click", () => {
-    let n = prompt("Please enter a number up to 100 to create a new grid.");
-    createGrid(n)
+    let newGrid = prompt("Please enter a number up to 100 to create a new grid.");
+    if (newGrid > 0 && newGrid < 101) {
+        reset(contentContainer);
+        createGrid(newGrid);
+    } else {
+        alert("Error: Please choose a number between 0-100.")
+    }
 });
-*/
+
+function reset(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+contentContainer.addEventListener("mouseover", (e) => {
+    const hoverDiv = e.target.classList;
+    if (!hoverDiv.contains("square") || hoverDiv.contains("orange")) return;
+    hoverDiv.add("orange")
+});
